d5bab2a10ba2fe15c53fd8df11aea28a9dad0cd7
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2019-04-01 10:57:27 -0600

    Add comments

--- data_exchange.asm
+++ data_exchange.asm
@@ -3,12 +3,12 @@
 collision_data: .fill 1,0
 	/*
 	collisions variable:
-	byte 0: rpad collision
-	byte 1: lpad collision
-	byte 2: upper border collision
-	byte 3: lower border collision
-	byte 4: right border collision
-	byte 5: left border collision
+	bit 0: rpad collision
+	bit 1: lpad collision
+	bit 2: upper border collision
+	bit 3: lower border collision
+	bit 4: right border collision
+	bit 5: left border collision
 	*/
 	.var collisions = collision_data
 
