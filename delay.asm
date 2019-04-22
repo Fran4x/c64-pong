a46784d5f8f81a762a199625d269cbefd5b528ab
Unknown <19davrockenzahn@asfg.edu.mx>
2019-04-21 19:06:07 -0500

    Add better documentation

--- delay.asm
+++ delay.asm
@@ -1,6 +1,9 @@
+
+	///subroutine to handle fixed time delay (busy wait)
+	///times ball speed in main loop
 delay:
-		//cycles formula = 5ab+5a
-	ldy#3 //a=10 (original value)
+		//cycles formula = 5ab+5a (total cycles)
+	ldy#3 //a=10 (original value, but 3 is better)
 	ldx#119 //b=119
 repeat:
 	
--- input_handling.asm
+++ input_handling.asm
@@ -11,50 +11,33 @@ handle_input:
 	lda #$00
 	sta ddrb // set data register b to input
 
-	jsr check_up
-	jsr check_down
+	///keyboard matrix scan
+
+	jsr check_up_keys //check for up key ('w' and 'o' combinations)
+	jsr check_down_keys //check for down key ('s' and 'l') combinations
 	rts
 
-check_s_o:
-
-	lda #%11101101
-	sta pra // select second and fifth keyboard rows
-
-	lda prb //check 's' and 'o'
-	and #%01100000
-	beq move_s_o
-	
-check_w_l:
-
-	lda #%11011101
-	sta pra // select third keyboard row
-
-	lda prb // load column info
-	and #%00000110 //check if 'w' and 'l' is pressed
-	beq move_w_l
-
-check_down:	
+check_down_keys:	
         lda #%11011101
 	sta pra // select third keyboard row
 
 
-
+	
 	lda prb // load column info
 	and #%00100100 //check if 's' and 'l' is pressed
-	beq move_down_2
+	beq try_move_down_both
 
 	lda prb // load column info
 	and #%00000100 //check if 's' is pressed
-	beq move_down
+	beq try_move_down_r
 
 	lda prb // load column info
 	and #%00100000 //check if 'l' is pressed
-	beq move_down_l
+	beq try_move_down_l
 	rts
 
-	//check up
 
-check_up:
+check_up_keys:
 	lda #%11101101
 	sta pra // select second and fifth keyboard rows
 
@@ -62,91 +45,70 @@ check_up:
 
 	lda prb // load column info
 	and #%01000010 // check if 'w' and 'o'  was pressed
-	beq move_up_2
+	beq try_move_up_both
 
 	lda prb // load column info
 	and #%00000010 // check if 'w' was pressed
-	beq move_up_l
+	beq try_move_up_l //l is for left
 
 	lda prb // load column info
 	and #%01000000 // check if 'o' was pressed
-	beq move_up
-	rts
-
-	//check down
-	
-
-	
-	//lda #%11011101
-	//sta pra // select third keyboard row
-
-	//lda prb // load column info
-	//and #%01100000 //check if 's' and 'o' is pressed
-	//beq move_up_2
-
-move_w_l:
-	jsr move_up_l
-	jsr move_down
+	beq try_move_up_r
 	rts
 	
-move_up:
+try_move_up_r: //try to move, if no border collision
 	lda $d001
 	cmp #$33
-	bcs move_up_dec // if position >= $33, then branch
+	bcs move_up_r // if position >= $33, then move
 	rts
 
-move_s_o:
-	jsr move_up_dec
-	jsr move_down_dec_l
+try_move_up_both:
+
+	jsr try_move_up_r
+	jsr try_move_up_l
 	rts
 
-move_up_2:
-
-	jsr move_up
-	jsr move_up_l
-	rts
-
-move_up_l:
+try_move_up_l:
 	lda $d003
 	cmp #$33
-	bcs move_up_dec_l // if position >= $33, then branch
+	bcs move_up_l // if position >= $33, then move
 	rts
 	
-move_up_dec:
+move_up_r:
 	dec $d001 //decrease sprite 1 y-coord
 	dec $d001
 	dec $d001
 	rts
 
-move_up_dec_l:
+move_up_l:
 	dec $d003 //decrease sprite 1 y-coord
 	dec $d003
 	dec $d003
 	rts
 
-move_down:
+try_move_down_r:
 	lda #$cf
 	cmp $d001
-	bcs move_down_dec // if position <= $cf, branch
+	bcs move_down_r // if position <= $cf, branch
 	rts
 
-move_down_l:
+try_move_down_l:
 	lda #$cf
 	cmp $d003
-	bcs move_down_dec_l // if position <= $cf, branch
+	bcs move_down_l // if position <= $cf, branch
 	rts
 
-move_down_dec:
+move_down_r:
 	inc $d001 //decrease sprite 1 y-coord
 	inc $d001
 	inc $d001
 	rts
-move_down_2:
-	jsr move_down
-	jsr move_down_l
+try_move_down_both:
+	jsr try_move_down_r
+	jsr try_move_down_l
 	rts
 
-move_down_dec_l:
+move_down_l:
 	inc $d003 //decrease sprite 1 y-coord
 	inc $d003
 	inc $d003
